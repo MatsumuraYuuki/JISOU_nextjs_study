8ff0f146bbc7920b3589a6a733940d27c987b115
MatsumuraYuuki <rikkyo.matumura@gmail.com>
2026-06-08 18:59:42 +0900

    start studying next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+import { hostname } from "os";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.microcms-assets.io',
+      }
+    ]
+  }
 };
 
 export default nextConfig;
---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,10 @@
+const Blogs = () => {
+  return (
+    <div>
+      <h1>ブログ一覧</h1>
+    </div>
+  )
+}
+
+export default Blogs
+
